6cf3babbd4cae3aa9a89be2e5d70f81290a31228
kelvin711 <arauzkelvin99@gmail.com>
2022-09-16 15:30:47 -0500

    string notes will continue on monday

--- Week_01/Day_05/strings.js
+++ Week_01/Day_05/strings.js
@@ -1,13 +1,16 @@
-/* In JavaSCript the type for representing text is the "string". A string is an immutable ordered sequence of the 16-bit values
+/* In javaScript the type for representing text is the "string". A string is an immutable ordered sequence of the 16-bit values
 Strings are "array like" and use 0 based indexing. */
 
 // STRING LITERALS
 // to create a string in JS we just enclose character of the string within "" or ''
+""
+'look a string can'
+`functions as a string`
 
 
 //what happens when we write a sentence like this --> 'you're late', she said
-// let string = 'you\'re late\nwoah we are on a new line'
-// console.log(string);
+let string = 'you\'re late\nwoah we are on a new line'
+console.log(string);
 
 
 
@@ -23,58 +26,63 @@ Strings are "array like" and use 0 based indexing. */
 
 // WORKING WITH STRINGS
 //string concatination, JS allows us to combine our string with the + operator
+let string1 = "im a string";
+let string2 = "im another string";
+console.log(string1 + " " + string2);
+console.log(string1[2]);
 
 
 //string length, we can access the length (number of characters in the string)
-
-
+console.log(string1.length);
 
 //SOME AWESOME METHODS FOR STRINGS
-let practice_with_some_strings = "Hello, world"
+//                                         321
+let practice_with_some_strings = "Hello, world";
 //an object wrapper around all primitives String(practice_with_some_strings)
 // let str = 'hello';
-// let temp = new String(str); // wrapper object
+// let temp = new String("new string"); // wrapper object
+// console.log(temp);
 // temp.custom = 1;
 // console.log(temp.custom);
 
 
 //obtaining portions of a string
 //substring and return "ell" ----->
-console.log(practice_with_some_strings.substring(1,4));  
+console.log(practice_with_some_strings.substring(1,4));
 //slice and return "worl" -----> 
-console.log(practice_with_some_strings.slice(7,-1));
+console.log(practice_with_some_strings.slice(7,-2));
 //split and return an array of both words -----> 
-console.log(practice_with_some_strings.split(", ").join(" "));
+
 //Bonus: what method can take an array of words and put them back together
 
 //searching a string
-console.log(practice_with_some_strings.indexOf("H"));
+
 //indexOf "H" ---->
-console.log(practice_with_some_strings.lastIndexOf("l"));
+
 //lastIndexOf "l" ----> 
 
 //boolean searching funtions
-console.log(practice_with_some_strings.startsWith("Hello"));
+
 //startsWith "Hello" gives us?---> 
-console.log(practice_with_some_strings.endsWith("!"));
+
 //endsWith "!" ---> 
-console.log(practice_with_some_strings.includes("no"));
+
 //includes "no" --->
 
 //creating modified version of a string
 //replace "llo" with "ya" ---> "Heya, world"
-console.log(practice_with_some_strings.replace("llo", "ya"));
+
 //toLowerCase ---> take a guess lol
-console.log(practice_with_some_strings.toLowerCase());
+
 //toUpperCase ---> take another guess
-console.log(practice_with_some_strings.toUpperCase());
+
 
 
 //inspecting individual characters of a string
 //charAt 0 ---> 
-console.log(practice_with_some_strings.charAt(0));
+
 //charCodeAt 0  --->
-console.log(practice_with_some_strings.charCodeAt(0));
+
 //Bonus: find the range of lower and uppercase letters
 
 
