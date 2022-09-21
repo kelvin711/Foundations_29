1734b1e2a624ece185f1eb5b2976ffa76868893f
kelvin711 <arauzkelvin99@gmail.com>
2022-09-20 23:10:58 -0500

    lecture notes ready for 9/20

--- Week_02/Day_01/functions.js
+++ Week_02/Day_01/functions.js
@@ -12,26 +12,27 @@
 //ANATOMY OF A FUNCTION
 
 //keyWord  name_of_function         parameters(if any)
-function your_first_function(name_input, favorite_number = 7) {
-    // if (name_input != typeof String) {
-    //     throw new Error("hey need a string you dingus for name")
-    // }
+// let obj = {}
+// obj.daniels_function = function your_first_function(name_input, favorite_number) {
+//     //body of function is inside the curly brackets
+//     let return_string = `your name is ${name_input}, and your fave num is ${favorite_number}`;
+//     // return undefined//if no return is stated the default is undefined
+//     return return_string
+// }
+function your_first_function(name_input, favorite_number) {
     //body of function is inside the curly brackets
     let return_string = `your name is ${name_input}, and your fave num is ${favorite_number}`;
-    //if no return is stated the default is undefined
-    console.log(`your name is ${name_input}, and your fave num is ${favorite_number}`);
-    // return `your name is ${name_input}, and your fave num is ${favorite_number}`;
-    //return undefined
+    // return undefined//if no return is stated the default is undefined
+    return return_string
 }
-
-let nice_sentence = your_first_function("comedy with out the c")
-console.log(nice_sentence);
-
-
+// console.log(obj.daniels_function("kelvin", 11))
+let name_argument = { name:"Kelvin" }
+let favorite_number_argument = 11
 //CALLING A FUNCTION
 //            name_of_function()----->arguments(if any)
-// console.log(your_first_function(name_argument,favorite_number_argument));
+console.log(your_first_function(name_argument,favorite_number_argument));
 //console logging functions prints a functions return statement'
+console.log("hello");
 
 
 //DEFINING FUNCTIONS
@@ -40,38 +41,29 @@ console.log(nice_sentence);
 //FUNCTION DECLERATIONS
 
 //key_word identifier(parameters)
-console.log(add_numbers(2,2));
-//FUNCTION DECLERATION EX
+console.log(add_numbers(2,2))
 function add_numbers(num1, num2) {
     //statements
     return num1 + num2
 }
-let four = add_numbers(2,2);
-// console.log(four);
+let four = add_numbers(2,2)
+console.log(add_numbers);
 //the name of the function becomes a variable whose value is the function
 
 //FUNCTION EXPRESSIONS
 //Function expressions look similar to function declarations, but they appear within the context 
 //of a larger expression or statement, and the name is optional. Here are some example 
 
-
-
 //assigning a function to a variable
-// console.log(square(10));
-
-//look up hoisting ill probably put something here later
-//addSquare() ~~~~
-//square; = function()
-
 const square = function(x) {
     return x * x;
 };
 console.log(square(10));
 
-
+// let sorted_array = [33,222,11,10].sort()
+// console.log(sorted_array);
 //function expressions can also be used as arguments to other functions
-
-console.log([3, 2, 1].sort( function(a, b) { return a - b; }));
+console.log([3, 2, 1].sort(function(a, b) { return a - b; }));
 
 //function expressions can be immediately invoked, neat
 let tensquared =
@@ -86,10 +78,6 @@ console.log(tensquared);
 // ARROW FUNCTIONS 
 // The function keyword is not used, and, since arrow functions are expressions instead of 
 // statements, there is no need for a function name, either
-
-// function sum(x,y){
-//     return x+y;
-// }
 
 const sum = (number1, number2) => {
     return number1 + number2;
@@ -115,25 +103,18 @@ const square_again = num_to_square => num_to_square ** 2
 //function scope
 //block scope
 
-//step 1
-//global_variable;
-//function hypotenuse() ~~~~~
-//step 2
-//global_variable = "this string can be seen by all"
-////function hypotenuse() ~~~~~
-
 
 let global_variable = "this string can be seen by all"
 
 function hypotenuse(a, b) {
-    
+
     function square(x) {
-        console.log(global_variable);
+
         // for (let i = 0; i < 3; i++) {
         //     //console.log(`we are block scoped thanks to i ${i}\n. a is: ${a} and b is: ${b}`);
             
         // }
-
+        // console.log(global_variable);
         return x * x;
 
     }
@@ -169,18 +150,19 @@ obj_example.hello_world()
 
 
 
-console.log("---------------------------");
 
+console.log("---------------------------");
 
 let o = {   
     //this = obj o              // An object o.
     m: function() {       // Method m of the object.
-        console.log(this === o)
+        console.log(this === o) //true
+        
         let f = () => {    // A nested function f
-            console.log(`this is: `, this);
-            console.log(this === o) 
+            console.log(`this is: ${this}`);
+            console.log(this === o) //false?
         }
         f();              // Now call the helper function f().
     }
 };
-o.m();   
+o.m();                // Invoke the method m on the object o.
---
+++ Week_02/Day_03/functions2.js
@@ -0,0 +1,144 @@
+//FUNCTIONS 2 ELECTRIC BOOGALOO EDITION
+
+//functional programming is a programming paradigm where programs are constructed by 
+//applying and composing functions. we can do this because functions return a value!
+
+let add = (x, y) => {
+    //return undefined
+    return x + y
+}
+
+let subtract = (x, y) => {
+    return x - y
+}
+
+const numA = add(54, 627)
+const numB = add(235, 8654)
+
+
+//neat lets cover
+//CALLBACK FUNCTIONS 
+// A callback is a function that you write and then pass to some other function. That other 
+// function then invokes (“calls back”) your function when some condition is met or some 
+// (asynchronous) event occurs
+
+setTimeout(() => console.log("that took a while"), 2000)
+
+//why use callbacks?
+// Callbacks make sure that a function is not going to run before a task is completed but 
+// will run right after the task has completed. It helps us develop asynchronous JavaScript 
+// code and keeps us safe from problems and errors
+
+function find_a_plus_b_squared(a, b, square_root_function) {
+    let value_to_root = a ** 2 + b ** 2
+    //once the value is taken we pass it to the square_root_function
+    return square_root_function(value_to_root);
+}
+
+function square_root_this_num(a_plus_b_squared) {
+    // Do what you need with the value
+    return Math.sqrt(a_plus_b_squared)
+}
+
+console.log(find_a_plus_b_squared(2,2,square_root_this_num));
+
+//                       string  f()
+const greetingMachine = (name, greeting) => {
+    return greeting(name)
+}
+
+const hello = name => {
+    console.log(`Hello, ${name}!`)
+}
+const sup = name => {
+    console.log(`'Sup, ${name}?`)
+}
+const yo = name => {
+    console.log(`Yo, ${name}!`)
+}
+const goodmorning = name => {
+    console.log(`Good morning, ${name}!`)
+}
+greetingMachine("Colin", yo);
+
+
+// we can also write callback functions inline
+function hypotenuse(a, b, square_function) {
+    return Math.sqrt(square_function(a) + square_function(b));
+}
+
+console.log(hypotenuse(2, 2, (n) => n ** 2));
+
+
+
+//HIGHER ORDER FUNCTION
+// A function that takes 1+ function(s) as an argument and/or returns a function
+// They have similar benefits to callbacks: flexibility and reusability
+// You’ve already seen them: they’re the outer functions in the callback examples
+
+function B() {
+    console.log("Inside the method B. called from B")
+}
+// first method that return second method
+function A() {
+    console.log("Method A being called")
+    // return second method
+    return B
+}
+
+let returned_function = A()
+
+// call second method by first method
+returned_function()
+
+// Higher Order Array Methods
+// Functions built into JavaScript that accept callbacks as arguments
+// We will cover forEach, map, filter, and reduce
+// Note: not all higher order functions have to work with arrays, there are others (setTimeout, addEventListener, etc.)
+
+let names = ["Anthony", "Alex", "Tiara", "Greg", "Amadi", "Jessica"]
+//DRY DONT REPEAT YOURSELF
+let each_name_loop = []
+for (let i = 0; i < names.length; i++) {
+    each_name_loop.push(names[i])
+}
+console.log(each_name_loop);
+
+let hello_names = []
+for (let i = 0; i < names.length; i++) {
+    hello_names.push(`hello ${names[i]}`)
+}
+console.log(hello_names);
+
+let each_name_index = []
+for (let i = 0; i < names.length; i++) {
+    each_name_index.push(`${names[i]} ${i}`)
+}
+console.log(each_name_index);
+
+//we are not executing DRY we are repeating core logic a lot with these three examples
+//the core logic is making a new array and then just pushing into it every iteration
+//this is what makes higher order functions powerful
+
+let each_name = names.map((one_name) => one_name)
+let hello_each_name = names.map((one_name) => "hello " + one_name)
+let each_name_and_index = names.map((one_name, idx) => one_name + " " + idx)
+
+
+
+
+
+// -----------------------------------------------------------------------------------
+// map under the hood
+
+
+let originalArray = [1, 2, 3, 4];
+
+function map(array, callback) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(callback(array[i], i, array))
+    };
+    return newArray
+}
+console.log(map(originalArray, (num) => num + 2 ));
